2cfc40b44ace3ca33a19a4f530f186570349d65a
florian935 <florian.martin63000@gmail.com>
2021-09-14 16:07:56 +0200

    Feat: added some demo of test with async support

--- src/app/component/twain-quote/twain-quote.component.spec.ts
+++ src/app/component/twain-quote/twain-quote.component.spec.ts
@@ -3,8 +3,9 @@ import {
     fakeAsync,
     TestBed,
     tick,
+    waitForAsync,
 } from '@angular/core/testing';
-import { of, throwError } from 'rxjs';
+import { defer, throwError } from 'rxjs';
 import { TwainService } from 'src/app/services/twain.service';
 import { TwainQuoteComponent } from './twain-quote.component';
 
@@ -24,7 +25,9 @@ describe('TwainQuoteComponent', () => {
         testQuote = 'Test Quote';
 
         const twainService = jasmine.createSpyObj('TwainService', ['getQuote']);
-        getQuoteSpy = twainService.getQuote.and.returnValue(of(testQuote));
+        getQuoteSpy = twainService.getQuote.and.returnValue(
+            defer(() => Promise.resolve(testQuote))
+        );
 
         TestBed.configureTestingModule({
             declarations: [TwainQuoteComponent],
@@ -46,13 +49,50 @@ describe('TwainQuoteComponent', () => {
             .toBe(false);
     });
 
-    it('should show quote after component initialized', () => {
+    it('should show quote after getQuote (fakeAsync)', fakeAsync(() => {
+        fixture.detectChanges();
+        expect(quoteEl.textContent)
+            .withContext('should show placeholder')
+            .toBe('...');
+
+        tick();
         fixture.detectChanges();
 
-        expect(quoteEl.textContent).toBe(testQuote);
-        expect(getQuoteSpy.calls.any())
-            .withContext('getQuote called')
-            .toBe(true);
+        expect(quoteEl.textContent)
+            .withContext('should show quote')
+            .toBe(testQuote);
+        expect(errorMessage()).withContext('should not show error').toBeNull();
+    }));
+
+    it(
+        'should show quote after getQuote (waitForAsync)',
+        waitForAsync(() => {
+            fixture.detectChanges();
+            expect(quoteEl.textContent)
+                .withContext('should show placeholder')
+                .toBe('...');
+
+            fixture.whenStable().then(() => {
+                fixture.detectChanges();
+                expect(quoteEl.textContent).toBe(testQuote);
+                expect(errorMessage())
+                    .withContext('should not show error')
+                    .toBeNull();
+            });
+        })
+    );
+
+    it('should show quote after getQuote (spy done)', (done: DoneFn) => {
+        fixture.detectChanges();
+
+        getQuoteSpy.calls.mostRecent().returnValue.subscribe(() => {
+            fixture.detectChanges();
+            expect(quoteEl.textContent).toBe(testQuote);
+            expect(errorMessage())
+                .withContext('should not show error')
+                .toBeNull();
+            done();
+        });
     });
 
     it('should display error when TwainService fails', fakeAsync(() => {
